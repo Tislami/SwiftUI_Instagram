2a4c38854ef0977e367fea0d10394ec60c1f9893
Tariyel <islami.tariyel@gmail.com>
2023-02-20 14:14:22 +0400

    Initial commit

---
+++ SwiftUI_Instagram/SwiftUI_InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_InstagramApp.swift
+//  SwiftUI_Instagram
+//
+//  Created by Tariyel Islami on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
